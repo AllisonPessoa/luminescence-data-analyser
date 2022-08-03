55ec7d5308eb0b6468274f21454835f2a2194b57
AllisonPessoa <66957124+AllisonPessoa@users.noreply.github.com>
2022-08-03 08:56:39 -0300

    Updates Hyperspectral class
    Probably needs changes after last commit

--- HyperspecAnalyser.py
+++ HyperspecAnalyser.py
@@ -7,9 +7,6 @@ import SpectraAnalyser as spr
 
 class Hyperspec():
     def __init__(self, fileName):
-        self._load(fileName)
-    
-    def _load(self, fileName):
         data = np.load(fileName, allow_pickle=True)
         wavelength = data[0]
         hyperData = data[1]
@@ -21,9 +18,45 @@ class Hyperspec():
             self.hyperSpectrum.append([])
             for j in range(self.dim):
                 self.hyperSpectrum[i].append(spr.Spectrum.loadDirectArray(wavelength, hyperData[i,j]))
-                self.hyperSpectrum[i][j].subtract_flat_baseline_by_region([588,590])
 
-        del data
+    def removeBackground(self, bgd_region):
+        for i in range(self.dim):
+            for j in range(self.dim):
+                self.hyperSpectrum[i][j].subtract_flat_baseline_by_region(bgd_region)
+    
+    def transpose(self):
+        self.hyperSpectrum = np.transpose(self.hyperSpectrum)
+        
+    def correctHisteresis(self, hist_pixels):
+        mask = []
+        
+        for i in range(self.dim):
+            mask.append([])
+            for j in range(self.dim):
+                if (i%2 == 0) and (j < self.dim - hist_pixels):
+                    mask[i].append(self.hyperSpectrum[i][j+hist_pixels])
+                else:
+                    mask[i].append(self.hyperSpectrum[i][j])
+    
+        self.hyperSpectrum = np.copy(mask)
+
+    def center_by_maximum_intensity(self, band_region):
+
+        mask = self.getIntensityMap('overall', band_region)
+        maximum  =  np.amax(mask)
+        loc = np.where(mask == maximum)
+        
+        mask2 = []
+        
+        for i in range(self.dim):
+            mask2.append([])
+            for j in range(self.dim):
+                try:
+                    mask2[i].append( self.hyperSpectrum[i - (int(self.dim/2)-int(loc[0][0]))][j - (int(self.dim/2)-int(loc[1][0]))] )
+                except:
+                    mask2[i].append(self.hyperSpectrum[-1][-1])
+         
+        self.hyperSpectrum = np.copy(mask2)
     
     def getIntensityMap(self, band_name, band_region):
         mask = []
@@ -33,10 +66,9 @@ class Hyperspec():
             for j in range(self.dim):
                 area = self.hyperSpectrum[i][j].add_band(band_name, band_region).get_area()
                 mask[i].append(area)
-        mask = np.transpose(mask)
-        
+    
         return mask
-        
+
     def getLIRMap(self, inf_band_name, inf_band_region, sup_band_name, sup_band_region, min_criteria):
         LIR = []
 
@@ -45,37 +77,26 @@ class Hyperspec():
             for j in range(self.dim):
                 area_sup = self.hyperSpectrum[i][j].add_band(sup_band_name, sup_band_region).get_area()
                 area_inf = self.hyperSpectrum[i][j].add_band(inf_band_name, inf_band_region).get_area()
-                
+
                 if (area_sup + area_inf) > min_criteria:
                     LIR[i].append(area_sup/area_inf)
                 else:
                     LIR[i].append(0)
-        LIR = np.transpose(LIR)
-        
+
         return LIR
-    
-    def correctHisteresis(self, mask, pixels):
-        mask2 = np.zeros((self.dim,self.dim))
-        self.hist_pixels = pixels
+
+
+    def getTempMap(self, inf_band_name, inf_band_region, sup_band_name, sup_band_region, alpha, beta):
+        Temp = []
+
         for i in range(self.dim):
-            for j in range(self.dim-pixels):
-                if i%2 == 0:
-                    mask2[i][j] = mask[i][j+pixels]
-                else:
-                    mask2[i][j] = mask[i][j]
-                    
-        return mask2
-    
-    def getSpectrum(self, k, l):
-        spec_mask = np.transpose(self.hyperSpectrum)
-        spec_mask2 = []
-    	
-        for i in range(self.dim):
-            spec_mask2.append([])
-            for j in range(self.dim-self.hist_pixels):
-                if i%2 == 0:
-                    spec_mask2[i].append(spec_mask[i][j+self.hist_pixels])
-                else:
-                    spec_mask2[i].append(spec_mask[i][j])
-                    
-        return spec_mask2[k][l]
+            Temp.append([])
+            for j in range(self.dim):
+                area_sup = self.hyperSpectrum[i][j].add_band(sup_band_name, sup_band_region).get_area()
+                area_inf = self.hyperSpectrum[i][j].add_band(inf_band_name, inf_band_region).get_area()
+
+                R = (area_sup/area_inf)
+                T = abs(alpha)*1/(beta - np.log(R))
+                Temp[i].append(T)
+            
+        return Temp
