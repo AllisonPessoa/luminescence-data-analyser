c9d31ab332e72a20d47084b5ad234fe6b5f4a4bb
Allison Pessoa <66957124+AllisonPessoa@users.noreply.github.com>
2025-02-15 15:48:33 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='SpectraAnaliser',
+    version='1.0',    
+    description='Code to analyse spectral data.',
+    url='https://github.com/AllisonPessoa/luminescence-data-analyser',
+    author='Allison Pessoa',
+    author_email='allison.pessoa@ufpe.br',
+    license='GNU GPL',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=['matplotlib',
+                      'scipy', 'numpy' 'PrettyTable', 'uncertainties'
+                      ],
+    package_data={'pcf_lib':['*',]}
+)
